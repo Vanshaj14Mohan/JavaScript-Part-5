8d09d20a680b4ccb9eed41f2ae842d426c12244c
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-18 01:32:49 +0530

    files

---
+++ Practice_Set/Practice_Set3/app.js
@@ -0,0 +1,8 @@
+//Create a function that gives us the average of 3 numbers.
+function avgNum(a,b,c){
+    let avg = (a+b+c)/3;
+    console.log("Average of three numbers are:", avg);
+}
+
+avgNum(2,4,6);
+avgNum(10,20,30);
