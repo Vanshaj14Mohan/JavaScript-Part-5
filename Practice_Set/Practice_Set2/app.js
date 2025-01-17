15f572298da647fc995440fd646e4a276ede5261
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-17 22:43:11 +0530

    files

--- Practice_Set/Practice_Set2/app.js
+++ Practice_Set/Practice_Set2/app.js
@@ -1,2 +1,8 @@
 //Create a function to roll a dice & always display the value of the dice:
- 
+ function rollDice(){
+    getRandom = Math.floor(Math.random()*6)+1;
+    console.log("Dice rolled:");
+    console.log("Value got after dice was rolled is:", getRandom);
+ }
+
+ rollDice();
