2944936e612c986ddea06e1293831a19ec8687e3
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-17 22:33:13 +0530

    files

---
+++ Practice_Set/Practice_Set2/app.js
@@ -0,0 +1,2 @@
+//Create a function to roll a dice & always display the value of the dice:
+ 
---
+++ Practice_Set/app.js
@@ -0,0 +1,6 @@
+//Write a function that prints a poem.
+function poemTheme(){
+    console.log("The cloud is raining down, raining down");
+}
+
+poemTheme();
