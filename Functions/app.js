890ad6e8962734c922fca3a1bb5e060917d530d0
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-18 16:50:50 +0530

    files

--- Functions/app.js
+++ Functions/app.js
@@ -62,3 +62,18 @@ function sum(a,b){
 
 sum(2,6);
 sum(4,8);
+
+//Using return keyword, not console.log()
+function checkSum(a,b){
+    console.log("Yes");
+    return a+b;
+    console.log("abc");
+}
+
+// console.log(checkSum(checkSum(2,4),4)); to print sum of three numbers using functions-> a trick 
+
+
+// let s = checkSum(2,5); will print the value only when we store it in a variable & then console.log()
+// or console.log(checkSum(2,5));
+// console.log(s);
+
