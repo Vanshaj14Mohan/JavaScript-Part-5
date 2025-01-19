56126c9f62ccfddcc3d322810eed1c113c8cf02d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-20 00:46:49 +0530

    Scope

--- Scope/app.js
+++ Scope/app.js
@@ -7,3 +7,34 @@ function calSum(a,b){
 // console.log(sum); we can't use sum outside of the function:
 calSum(2,6);
 console.log(sum); //will print 20
+
+//Using Block Scope
+{
+    let a = 10;
+    const b = 20;
+    console.log("Using let keyword:", a); 
+    console.log("Using const keyword:",b); 
+}
+//will give error coz variable is defined under a block scope
+// console.log(a); 
+// console.log(b); 
+
+//but if we use var, block scope does'nt apply on var keyword
+{
+    var a = 15;
+}
+
+console.log("Using var keyword:", a);
+
+console.log("Using Lexical Scope");
+function outerFunc(){ //function scope
+    let x = 5;
+    let y = 6;
+    function innerFunc(){ //function scope
+        console.log("Value of x after lexical scope", x);
+        console.log("Value of y after lexical scope", y);
+    }
+    innerFunc();
+}
+
+outerFunc();
