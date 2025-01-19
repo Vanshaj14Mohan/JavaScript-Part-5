1cc60ccd7b7a0045e9f889fcfc7f66d989aa0ff5
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-19 23:53:22 +0530

    Scope

--- Scope/app.js
+++ Scope/app.js
@@ -1,6 +1,9 @@
+let sum = 20; //Global Scope -> Can be used anywhere in the code.
+
 function calSum(a,b){
     let sum = a+b;
-    console.log(sum); //Function Scope
+    console.log(sum); //Function Scope and if we did'nt use sum variable it will print 20 too.
 }
 // console.log(sum); we can't use sum outside of the function:
-calSum(2,5);
+calSum(2,6);
+console.log(sum); //will print 20
